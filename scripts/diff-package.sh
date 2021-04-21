25c9b43e7e5adaadbee3b24c31f6c2038cdc6af1
Christoph Raaflaub <raaflaub@puzzle.ch>
2021-04-21 09:30:38 +0200

    fix package diff

--- scripts/diff-package.sh
+++ scripts/diff-package.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+echo "## diff-package.sh ##"
+
+if (($# == 0)); then
+  echo "no input, using default values"
+  REPOS_PATH=repos
+  BASE_REPO="acend/hugo-training-template"
+  REPO="puzzle/amm-techlab"
+  VISUAL_DIFF=true
+else
+  REPOS_PATH=$1
+  BASE_REPO=$2
+  REPO=$3
+  VISUAL_DIFF=$4
+fi
+
 FILE=package.json
-REPOS_PATH=repos
+IGNORE_LINES='author description homepage name url version'
+IGNORE_GREP=""
+for value in $IGNORE_LINES; do
+  IGNORE_GREP+="(\")${value}+(\":).*|"
+done
+IGNORE_GREP_STRING="${IGNORE_GREP::-1}"
 
-diff \
-  -I '"author":.*' \
-  -I '"description":.*' \
-  -I '"homepage":.*' \
-  -I '"name":.*' \
-  -I '"url":.*' \
-  -I '"version":.*' \
-  ${REPOS_PATH}/acend/hugo-training-template/${FILE} ${REPOS_PATH}/puzzle/amm-techlab/${FILE}
+diff -B \
+  <(grep -vE $IGNORE_GREP_STRING ${REPOS_PATH}/${BASE_REPO}/${FILE}) \
+  <(grep -vE $IGNORE_GREP_STRING ${REPOS_PATH}/${REPO}/${FILE})
 
-meld ${REPOS_PATH}/acend/hugo-training-template/${FILE} ${REPOS_PATH}/puzzle/amm-techlab/${FILE}
+if [ "$VISUAL_DIFF" = true ]; then
+  meld ${REPOS_PATH}/${BASE_REPO}/${FILE} ${REPOS_PATH}/${REPO}/${FILE}
+fi
