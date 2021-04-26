d41bc62b55ff3ca58505cfb50748d7caaa4eae51
Christoph Raaflaub <raaflaub@puzzle.ch>
2021-04-26 10:38:45 +0200

    repo diff with gitignore

--- scripts/diff-gitignore.sh
+++ scripts/diff-gitignore.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-FILE=.gitignore
-SCRIPT_DIR=$(dirname $0)
+echo "## diff-gitignore.sh ##"
 
-./${SCRIPT_DIR}/diff.sh ${FILE}
+if (($# == 0)); then
+  echo "no input, using default values"
+  REPOS_PATH=repos
+  BASE_REPO="acend/hugo-training-template"
+  REPO="puzzle/amm-techlab"
+  VISUAL_DIFF=true
+else
+  REPOS_PATH=$1
+  BASE_REPO=$2
+  REPO=$3
+  VISUAL_DIFF=$4
+fi
+
+FILE=.gitignore
+
+diff ${REPOS_PATH}/${BASE_REPO}/${FILE} ${REPOS_PATH}/${REPO}/${FILE}
+
+if [ "$VISUAL_DIFF" = true ]; then
+  meld ${REPOS_PATH}/${BASE_REPO}/${FILE} ${REPOS_PATH}/${REPO}/${FILE}
+fi
---
+++ scripts/diff-repos.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo 0 $0
+echo 1 $1
+echo 2 $@
+echo h $#
+
+
+BASE_REPO="acend/hugo-training-template"
+REPOS="puzzle/amm-techlab"
+VISUAL_DIFF=false
+REPOS_PATH=repos
+SCRIPT_DIR=$(dirname $0)
+FILES="package gitignore"
+
+# functions
+
+diff_files () {
+    repo=$1
+    for file in $FILES; do
+        echo "## compare file: ${file} (${repo})##"
+        ./${SCRIPT_DIR}/diff-${file}.sh ${REPOS_PATH} ${BASE_REPO} ${repo} ${VISUAL_DIFF}
+        echo ""
+    done
+}
+
+diff_repos () {
+    for repo in $REPOS; do
+        echo "######################################################"
+        echo "## compare repo: ${repo} ##"
+        echo ""
+        diff_files ${repo}
+        echo ""
+        echo ""
+    done
+}
+
+# diff
+
+diff_repos
