ccf5ce5da01a8b37d6149a406d90d0bc25f602b6
Christoph Raaflaub <raaflaub@puzzle.ch>
2021-04-26 10:54:16 +0200

    diff output to stdout and report files

--- scripts/diff-repos.sh
+++ scripts/diff-repos.sh
@@ -3,6 +3,7 @@
 BASE_REPO="acend/hugo-training-template"
 REPOS="puzzle/amm-techlab"
 VISUAL_DIFF=false
+REPORTS_PATH=reports
 REPOS_PATH=repos
 SCRIPT_DIR=$(dirname $0)
 FILES="dockerfile gitignore markdownlint package"
@@ -13,7 +14,7 @@ diff_files () {
     repo=$1
     for file in $FILES; do
         echo "## compare file: ${file} (${repo})##"
-        ./${SCRIPT_DIR}/diff-${file}.sh ${REPOS_PATH} ${BASE_REPO} ${repo} ${VISUAL_DIFF}
+        ./${SCRIPT_DIR}/diff-${file}.sh ${REPOS_PATH} ${BASE_REPO} ${repo} ${VISUAL_DIFF} | tee ${REPORTS_PATH}/${repo}/${file}.diff
         echo ""
     done
 }
@@ -23,6 +24,7 @@ diff_repos () {
         echo "######################################################"
         echo "## compare repo: ${repo} ##"
         echo ""
+        mkdir -p ${REPORTS_PATH}/${repo}
         diff_files ${repo}
         echo ""
         echo ""
