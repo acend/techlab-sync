815e46c54d097f4074feb85d6954a82ecfa2b025
Christoph Raaflaub <raaflaub@puzzle.ch>
2021-05-10 11:12:55 +0200

    add acend container-basics-training helm-basics-training repos

--- scripts/diff-action.sh
+++ scripts/diff-action.sh
@@ -19,6 +19,12 @@ FILE=.github/workflows/
 
 diff \
   -I '#.*' \
+  -I 'acend version.*' \
+  -I 'args:.*' \
+  -I 'image-ref:.*' \
+  -I 'namespace:.*' \
+  -I 'puzzle versio.*' \
+  -I 'quay.io.*' \
   ${REPOS_PATH}/${BASE_REPO}/${FILE} ${REPOS_PATH}/${REPO}/${FILE}
 
 if [ "$VISUAL_DIFF" = true ]; then
