2096d6aff44e67accb29fff8d28709df942b5ab0
Christoph Raaflaub <raaflaub@puzzle.ch>
2021-07-29 11:32:24 +0200

    diff renovate against fix template

--- scripts/diff-renovate.sh
+++ scripts/diff-renovate.sh
@@ -4,6 +4,8 @@ source $(dirname $0)/_functions.sh
 check_input $@
 
 FILE=renovate.json
-diff ${REPOS_PATH}/${BASE_REPO}/${FILE} ${REPOS_PATH}/${REPO}/${FILE}
+# diff ${REPOS_PATH}/${BASE_REPO}/${FILE} ${REPOS_PATH}/${REPO}/${FILE}
+# diff against fix template
+diff templates/renovate.json ${REPOS_PATH}/${REPO}/${FILE}
 
 after_diff
