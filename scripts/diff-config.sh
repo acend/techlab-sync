2d48be4a08efc4b26edc03f572554490a3ed1987
Christoph Raaflaub <raaflaub@puzzle.ch>
2021-07-29 13:01:40 +0200

    config diff excludes

--- scripts/diff-config.sh
+++ scripts/diff-config.sh
@@ -4,10 +4,16 @@ source $(dirname $0)/_functions.sh
 check_input $@
 
 FILE=config/_default/config.toml
-diff \
-  -I 'github_repo.*' \
-  -I 'title.*' \
-  -I 'url.*' \
-  ${REPOS_PATH}/${BASE_REPO}/${FILE} ${REPOS_PATH}/${REPO}/${FILE}
+
+IGNORE_LINES='copyright github_branch github_repo languageName modeSwitcher theme title url "# Enable expert mode"'
+IGNORE_GREP=""
+for value in $IGNORE_LINES; do
+  IGNORE_GREP+="${value}.*|"
+done
+IGNORE_GREP_STRING="${IGNORE_GREP::-1}"
+
+diff -B \
+  <(grep -vE $IGNORE_GREP_STRING ${REPOS_PATH}/${BASE_REPO}/${FILE}) \
+  <(grep -vE $IGNORE_GREP_STRING ${REPOS_PATH}/${REPO}/${FILE})
 
 after_diff
